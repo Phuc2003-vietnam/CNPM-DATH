f911ede3c9fa33de4d6dd7725916cb1669ca06db
hungvo2003vn <votanhung2003@gmail.com>
2023-11-15 05:31:40 +0700

    Ticket-28-Update printingLog Model

--- src/model/printingLog.js
+++ src/model/printingLog.js
@@ -4,13 +4,22 @@ const printingLog = new Schema(
   {
     status: { type: String, default: "default"},
     finishDate: { type: Date, default: Date.now},
+    
     paperSize: { type: String, default: "default", },
     numVersion: { type: Number, default: 0  },
+    colorOption: {type: Boolean, default: false},
+    landScapeOption : {type: Boolean, default: false},
+    pagesPerSheet: {type: Number, default: 1},
+
     document: {
       title:  { type: String, default: "default", maxLength: 20}, 
       pages:  { type: Number, default: 0}, 
       fileType:  { type: String, default: "default", maxLength: 20}, 
-    }
+    },
+    //Foreign Key
+    user_id: {type:String, default: "default"}, 
+    printerId: {type: String, default: 'default', maxLength: 50},
+    
   },
   {
     timestamps: true,
