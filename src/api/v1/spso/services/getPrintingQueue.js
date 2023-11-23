ec2d21a8886dffca729fee02bd4ffba14ebc18ce
PhucNguyen-1807 <thienphuc532266@gmail.com>
2023-11-23 13:45:13 +0700

    Fix forget printerId wrong value

--- src/api/v1/spso/services/getPrintingQueue.js
+++ src/api/v1/spso/services/getPrintingQueue.js
@@ -3,7 +3,13 @@ import getPrintingRecordHelper from './getPrintingRecordHelper.js'
 
 async function getPrintingQueue({printerId}) {
 	var printerRecord = await printer.findOne({printerId}).select('-printingLog')
-
+	if(!printerRecord)
+	{
+		return Promise.reject({
+			status: 400,
+			message: 'Wrong printerId value or not assigned value',
+		})
+	}
 	const printerObject = printerRecord.toObject()
 	printerObject.printingQueue = await getPrintingRecordHelper(printerObject.printingQueue) //Join PrintingQueue
 	printerObject.printingJob = await getPrintingRecordHelper(printerObject.printingJob) //Join PrintingJob
--- src/api/v1/staff/services/getPrintingLog.js
+++ src/api/v1/staff/services/getPrintingLog.js
@@ -3,7 +3,13 @@ import getPrintingRecordHelper from './getPrintingRecordHelper.js'
 
 async function getPrintingQueue({printerId}) {
 	var printerRecord =  await printer.findOne({printerId}).select('-printingJob -printingQueue')
-
+	if(!printerRecord)
+	{
+		return Promise.reject({
+			status: 400,
+			message: 'Wrong printerId value or not assigned value',
+		})
+	}
 	const printerObject = printerRecord.toObject()
 	printerObject.printingLog = await getPrintingRecordHelper(printerObject.printingLog)
 	return printerObject
