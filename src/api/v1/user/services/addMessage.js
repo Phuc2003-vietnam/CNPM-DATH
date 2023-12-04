4fefb015c0f1a14331f8097c4ffbdb3dcdc44c9d
PhucNguyen-1807 <thienphuc532266@gmail.com>
2023-12-04 09:04:59 +0700

    fix bug

--- src/api/v1/user/services/addMessage.js
+++ src/api/v1/user/services/addMessage.js
@@ -1,39 +1,42 @@
 import message from '#~/model/message.js'
 import conversation from '#~/model/conversation.js'
-import user from '#~/model/user.js'
 async function addMessage({conversationId, senderId, text}) {
-	//Handle empty message
-	if (text.length == 0) {
-		return Promise.reject({
-			status: 403,
-			message: 'Empty text, enter a value',
-		})
-	}
-	const data = await conversation.findById(conversationId)
-
-	if (data) {
-	//Handle sender not belong to a conversation
-
-		const members = data[0].members
-		if (members.includes(senderId)) {
+	try {
+		//Handle empty message
+		if (text.length == 0) {
 			return Promise.reject({
 				status: 403,
-				message: `Your account doesnt belong to this conversation id`,
+				message: 'Empty text, enter a value',
 			})
 		}
-		//create message
-		const messageRecord = await message.create({
-			conversationId,
-			senderId,
-			text,
-		})
-		return messageRecord
-	} else {
-		//Handle conversationId wrong value
-		return Promise.reject({
-			status: 403,
-			message: `The conversationId is not correct you send: ${conversationId}`,
-		})
+		const data = await conversation.findById(conversationId)
+		if (data) {
+			//Handle sender not belong to a conversation
+
+			const members = data.members
+			if (members.includes(senderId)) {
+				return Promise.reject({
+					status: 403,
+					message: `Your account doesnt belong to this conversation id`,
+				})
+			}
+			//create message
+			const messageRecord = await message.create({
+				conversationId,
+				senderId,
+				text,
+			})
+			return messageRecord
+		} else {
+			//Handle conversationId wrong value
+			return Promise.reject({
+				status: 403,
+				message: `The conversationId is not correct you send: ${conversationId}`,
+			})
+		}
+	} catch (err) {
+		console.log(err)
+		return Promise.reject({status: 500, message: 'System Error'})
 	}
 }
 
